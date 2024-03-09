7563b381549ac674003a0ce46966e441d4e5bb2a
Alexandre Sta <sta.aalexandre@gmail.com>
2024-03-09 23:11:17 +0100

    Add header and use new secript for function & global variables

--- app.R
+++ app.R
@@ -1,18 +1,16 @@
-# Description : 
-# 
-# mastermind colour :
-# green : "#43caa7"
-# blue : "#000bdf"
-# red : "#df0000"
-# violet : "#701abf"
-# yellow :"#dfbe00"
-# orange : "#ff7e31"
-# white : "#ffffff"
-# pink : "#df5abe"
-
+# Description :
 
 
 # Libraries ---------------------------------------------------------------
+### ///////////////////////////////////////////////////////////////////////
+# Script name : app.R
+# Script status : WIP
+# Script description : Build the shiny app to be able to play mastermind and to
+#                      test algorithm that solves master mind. 
+#                      The app has two panels, a game panel which code the 
+#                      classical mastermind. A test panel that enable the user 
+#                      to load a script that solve the the game.
+### ///////////////////////////////////////////////////////////////////////
 
 
 library(shiny)
@@ -24,155 +22,67 @@ library(tidyverse)
 options(shiny.reactlog = TRUE)
 
 # Load functions ----------------------------------------------------------
-btnCLickNDisplay <- function(input, output, colour, btnName){
-  
-  # Initialize button variables
-  clickCount <- reactiveVal(0)
-  
-  # Update click variables
-  observeEvent(input[[btnName]], {
-    clickCount(clickCount() + 1)
-  })
-  
-  # Build the button parameters
-  button_colour <- reactive(colour[clickCount() %% 8 + 1])
-  button_style <- reactive(
-    paste0(
-      "color: white;
-          background-color: ",
-      button_colour(),
-      ";
-          position: relative;
-          left: 3%;
-          height: 35px; 
-          width: 35px;
-          text-align:center;
-          text-indent: -2px;
-          border-radius: 50%;
-          border-width: 2px"
-    )
-  )
-  
-  # Display the buttons
-  output[[btnName]] <-
-    renderUI({
-      actionButton(inputId = btnName,
-                   label = "",
-                   style = button_style())
-    })
-  
-  # Extract colour
-  btnColourName = paste0(btnName,"Colour")
-  output[[btnColourName]] = clickCount
-  
-  # Build output
-  return( clickCount )
-}
 
-# buttons = reactiveVar(c(btn1(),btn2(),btn3(),btn4()))
+source("fct4app.r")
 
-btnPageantry <- function(input,output,buttons){
-  
-  for(btn in buttons){
-    button_style <- reactive(
-      paste0(
-        "color: white;
-          background-color: ",
-        btn(),
-        ";
-          position: relative;
-          left: 3%;
-          height: 35px; 
-          width: 35px;
-          text-align:center;
-          text-indent: -2px;
-          border-radius: 50%;
-          border-width: 2px"
-      )
-    )
-    
-    insertUI(
-      selector = "#my_buttons",
-    )
-    
-    output[[btnName]] <-
-      renderUI({
-        actionButton(inputId = btnName,
-                     label = "",
-                     style = button_style())
-      })
-    
-  }
-  
-  
-  
-  return(NULL)
-}
+
+# Load global variables ---------------------------------------------------
+
+source("globalVar.R")
 
 # UI ----------------------------------------------------------------------
 
-ui <- fluidPage(
-  titlePanel("My great mastermind !!!"),
-  sidebarLayout(
-    mainPanel(
-      titlePanel("toto"),
-      plotOutput("my_plot")
-    ),
-    sidebarPanel(
-      uiOutput(style = "display: inline-block", "button1"),
-      uiOutput(style = "display: inline-block", "button2"),
-      uiOutput(style = "display: inline-block", "button3"),
-      uiOutput(style = "display: inline-block", "button4"),
-      actionButton("reset_button", "Submit")
-    )
-  )
-)
+ui <- fluidPage(titlePanel("My great mastermind !!!"),
+                sidebarLayout(
+                  mainPanel(titlePanel("toto"),
+                            plotOutput("my_plot")),
+                  sidebarPanel(
+                    uiOutput(style = "display: inline-block", "button1"),
+                    uiOutput(style = "display: inline-block", "button2"),
+                    uiOutput(style = "display: inline-block", "button3"),
+                    uiOutput(style = "display: inline-block", "button4"),
+                    actionButton("reset_button", "Submit")
+                  )
+                ))
 
 # Server ------------------------------------------------------------------
 
-
 server <- function(input, output) {
-  # Initialize global variables
-  colour <-
-    c(
-      "#43caa7",
-      "#000bdf",
-      "#df0000",
-      "#701abf",
-      "#dfbe00",
-      "#ff7e31",
-      "#ffffff",
-      "#df5abe"
-    )
-  
-  # Build the button 
-  btn1 <- btnCLickNDisplay(input, output, colour, btnName = "button1" )
-  btn2 <- btnCLickNDisplay(input, output, colour, btnName = "button2" )
-  btn3 <- btnCLickNDisplay(input, output, colour, btnName = "button3" )
-  btn4 <- btnCLickNDisplay(input, output, colour, btnName = "button4" )
-  
+  # Build the button
+  btn1 <- btnCLickNDisplay(input, output, btnName = "button1")
+  btn2 <- btnCLickNDisplay(input, output, btnName = "button2")
+  btn3 <- btnCLickNDisplay(input, output, btnName = "button3")
+  btn4 <- btnCLickNDisplay(input, output, btnName = "button4")
   
   # Reactive data frame
-  df <- reactiveVal(tibble(btn1 = 0, btn2 = 0, btn3 = 0, btn4 = 0, `Try n°` = 0))
+  df <-
+    reactiveVal(tibble(
+      btn1 = 0,
+      btn2 = 0,
+      btn3 = 0,
+      btn4 = 0,
+      `Try n°` = 0
+    ))
   observeEvent(input[["reset_button"]],
                {
                  # Update the data frame
-                 df_data <- rbind(df(), c(btn1(), btn2(), btn3(), btn4())) %>% mutate(`Try n°` = row_number())
+                 df_data <-
+                   rbind(df(), c(btn1(), btn2(), btn3(), btn4())) %>% mutate(`Try n°` = row_number())
                  df_data <- df_data
                  df(df_data)
                  
-                 # Add Pageantry button on the main pannel to show the combinaisons played
+                 # Add Pageantry button on the main panel to show the combinations played
                  
                  # Print the data frame storing all the try
-                 print( df_data %>%
-                 pivot_longer(1:4,names_to = "btnID",values_to = "colour") %>%
-                 mutate(
-                   btnID = str_replace(btnID, "btn", "")
-                   ),
-                 colourDF = colour[colourDF %% 8 +1]
-                 )
-               }
-               )
+                 if (TRUE) { # TRUE : for debugging, FALSE : to have a clean app.
+                   print(
+                     df_data %>%
+                       pivot_longer(1:4, names_to = "btnID", values_to = "colour") %>%
+                       mutate(btnID = str_replace(btnID, "btn", "")),
+                     colourDF = colour[colourDF %% length(colour) + 1]
+                   )
+                 }
+               })
   
   # Plot the game
   # output$my_plot <- renderPlot({
